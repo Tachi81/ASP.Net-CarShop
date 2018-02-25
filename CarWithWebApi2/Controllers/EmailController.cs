411972a0f4a64bef6503a9b000fae0335ea8986c
Daniel Rużewicz <d.ruzew@gmail.com>
2018-02-25 17:05:58 +0100

    In project with API created EmailAPIModel, reworked EmailService to work with it. Aldo added EmailController and adjusted Post part of it

---
+++ CarWithWebApi2/Controllers/EmailController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CarShop.Service;
+using CarWithWebApi2.Models;
+
+namespace CarWithWebApi2.Controllers
+{
+    public class EmailController : ApiController
+    {
+        // GET: api/EmailApi
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/EmailApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/EmailApi
+        [ResponseType(typeof(EmailApiModel))]
+        public IHttpActionResult PostPerformance(EmailApiModel emailApiModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var service = new EmailService();
+            service.SendEmail(emailApiModel);
+
+
+            return Ok();
+        }
+
+        // PUT: api/EmailApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/EmailApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CarWithWebApi2/Models/EmailApiModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarWithWebApi2.Models
+{
+    public class EmailApiModel
+    {
+        public string Topic { get; set; }
+        public string Body { get; set; }
+        public string To { get; set; }
+    }
+}
